603c44226ed5162e579f3319877440a808a6ca9d
psparks92 <psparks1992@gmail.com>
2016-10-31 18:12:31 -0500

    attrpipe in the works

--- app/players/player-detail.component.ts
+++ app/players/player-detail.component.ts
@@ -4,14 +4,14 @@ import { OnInit } from 'angular2/core';
 import {Http, Response} from 'angular2/http';
 import {IPlayer} from './player';
 import {PlayerService} from './player.service';
-import{playerFilterPipe, KeysPipe} from './player-filter.pipe';
+import{playerFilterPipe, KeysPipe, AttributesPipe} from './player-filter.pipe';
 import {Observable} from 'rxjs/Observable'
 
 @Component({
     selector: 'pm-player',
     templateUrl: 'app/players/player-detail.component.html',
     providers: [PlayerService],
-    pipes : [KeysPipe]
+    pipes : [KeysPipe, AttributesPipe]
 })
 export class PlayerDetailComponent implements OnInit{
     pageTitle: string = 'Player Detail';
--- app/players/player-filter.pipe.ts
+++ app/players/player-filter.pipe.ts
@@ -1,19 +1,28 @@
 import { PipeTransform, Pipe } from 'angular2/core';
-import {IPlayer} from './player';
+import { IPlayer, Attribute } from './player';
 
 @Pipe({
     name: 'playerFilter'
 })
 
 export class playerFilterPipe implements PipeTransform {
-    transform(value: IPlayer[], args: string[]) : IPlayer[] {
-	let filter: string = args[0] ? args[0].toLocaleLowerCase() : null;
-	return filter ? value.filter((player: IPlayer) =>
-	player.firstName.toLocaleLowerCase().indexOf(filter) != -1 
-	|| player.lastName.toLocaleLowerCase().indexOf(filter) != -1) : value;
+    transform(value: IPlayer[], args: string[]): IPlayer[] {
+        let filter: string = args[0] ? args[0].toLocaleLowerCase() : null;
+        return filter ? value.filter((player: IPlayer) =>
+            player.firstName.toLocaleLowerCase().indexOf(filter) != -1
+            || player.lastName.toLocaleLowerCase().indexOf(filter) != -1) : value;
     }
 }
-@Pipe({name: 'keys'})
+@Pipe({ name: 'attributeDisplay' })
+export class AttributesPipe implements PipeTransform {
+    transform(attr: Attribute): [string, number] {
+        let name = attr.name,
+            value = attr.value;
+        return [name.substring(14), value];
+    }
+
+}
+@Pipe({ name: 'keys' })
 export class KeysPipe implements PipeTransform {
     transform(value: any, args?: any[]): any[] {
         let keys = Object.keys(value),
@@ -26,3 +35,5 @@ export class KeysPipe implements PipeTransform {
         return data;
     }
 }
+
+
